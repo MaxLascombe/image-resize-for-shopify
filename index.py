1d741dcc3a70fb09bb57d66cac14943123ccdb5e
Max Lascombe <mtl2154@columbia.edu>
2024-06-27 16:00:43 +0800

    Added ability to recursively copy directories and process files in those directories.

--- index.py
+++ index.py
@@ -3,28 +3,47 @@ from PIL import Image
 import math
 
 directory = os.fsencode('in')
+dirs = ['']
 
-file_type = 'jpg'
+file_types = ['.jpg', '.jpeg', '.png']
 
 files = []
 
-for file in os.listdir(directory):
-    filename = os.fsdecode(file)
-    if filename.endswith(file_type):
-        files.append(filename)
+while len(dirs) > 0:
+    dirPath = dirs.pop(0)
+    inDir = f"in{dirPath}" if dirPath != '' else 'in'
+    outDir = f"out{dirPath}" if dirPath != '' else 'out'
+    for file in os.listdir(inDir):
+        filename = os.fsdecode(file)
+        if filename.startswith('.'):
+            continue
+        if os.path.isdir(f"{inDir}/{filename}"):
+            try:
+                os.mkdir(f"{outDir}/{filename}")
+            except:
+                print(f"Directory {outDir}/{filename} already exists")
+            dirs.append(f"{dirPath}/{filename}")
+            continue
+        for file_type in file_types:
+            if filename.endswith(file_type):
+                files.append(f"{dirPath}/{filename}")
+                break
 
 goal = 19_999_999
 
-for file in files:
-    print(file)
-    if file == '.gitkeep':
-        continue
-    img_in = Image.open(f"in/{file}") 
+def resize_image(path):
+    img_in = Image.open(f"in/{path}")
 
     width, height = img_in.size 
     pixels = width * height
     new_width = math.floor(width * math.sqrt(goal / pixels)) if pixels > goal else width
     new_height = math.floor(height * math.sqrt(goal / pixels)) if pixels > goal else height
     img_out = img_in.resize((new_width, new_height))
-    img_out.save(f"out/{file}")
-    print(f"{file}: {pixels/1_000_000}MP ({width}x{height}) -> {new_width*new_height/1_000_000}MP ({new_width}x{new_height})")
+    img_out.save(f"out/{path}")
+    print(f"{path}: {pixels/1_000_000}MP ({width}x{height}) -> {new_width*new_height/1_000_000}MP ({new_width}x{new_height})")
+
+
+for file in files:
+    if file == 'in/.gitkeep':
+        continue
+    resize_image(file)
